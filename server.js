c5b418b16b3f33f2ca5031df8e6eb80b23326c5e
gorsyntora <aldownar@gmail.com>
2021-12-29 21:26:56 +0300

    termserv

--- server.js
+++ server.js
@@ -3,29 +3,65 @@
 
 var express = require('express')
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
 var app = express()
 
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+secret:'12345'
+
+})
+);
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // parse application/json
+
 app.use(bodyParser.json())
 
-app.get('/', function (req, res) {
 
+
+app.use(function (req, res, next){
+
+if (req.token===undefined) {   req.token = 0 }  else { req.token++ };
+
+req.session.counter = req.session.counter || 0;
+  req.session.counter += 1;
+
+console.log('midleware  token:  '+ JSON.stringify(req.session.counter, null, 4));
+next();
+
+
+}
+)
+
+app.get ('/app',function (req, res) {
+console.log ('get app');
+
+  res.send('app get request '+JSON.stringify(req.session.counter, null, 4))
+})
+
+
+app.get('/', function (req, res) {
+console.log ('get route');
 res.sendfile('index.html')
 //  res.send('root server myserver')
 })
 
 
+
+
 app.post('/',(req, res) => {
-console.log('post resource:  '+ JSON.stringify(req.body, null, 4));
-console.log('resource:  '+ req.body.fname);
+
+console.log('post session:  '+ JSON.stringify(req.session.counter, null, 4));
+console.log('post Body:  '+ JSON.stringify(req.body, null, 4));
+console.log('FName  :  '+ req.body.fname);
 }
 )
 
 app.listen(3000, async () => {
+
   console.log('Server ready!')
    // const mongoConnection = await mongoose.connect('mongodb://localhost/nodejs-auth');
 //  console.log('Database ready!')
---
+++ test.js
@@ -0,0 +1,5 @@
+var mag = function () {
+a;
+};
+mag.b= 14;
+ if (mag.a!==undefined) console.log(mag.a);
